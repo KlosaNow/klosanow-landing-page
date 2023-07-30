e21c16dfd6655096ec7858dfb354902d59afca4c
sammy-code98 <sammysmart98@yahoo.com>
2023-07-30 03:19:51 +0100

    feat: add pages

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+export default function AboutPage() {
+    return (
+        <>
+            <Box> about page</Box>
+
+        </>
+    )
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+
+
+export default function ContactPage() {
+    return (
+        <>
+            <Box> contact page</Box>
+
+        </>
+    )
+}
