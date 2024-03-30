7b2e782be594b359e9f4cae1df25118333caf37e
Jmcglobal <mmadubugwuchibuife@gmail.com>
2024-03-30 16:27:58 +0100

    added-script

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,4 @@ set -e
 sudo chown ubuntu:root -R /home/ubuntu/klosanow/landing
 sudo systemctl start klosanow-landing.service
 
-sleep 5s
+sleep 20s
--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sleep 20s
+sleep 30s
 # Check if port 3001 is running
 if ! nc -z localhost 3000; then
   echo "Port 3000 is not running"
